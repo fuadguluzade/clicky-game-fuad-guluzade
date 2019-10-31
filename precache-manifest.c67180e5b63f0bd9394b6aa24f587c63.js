c2fcf3a233f1596393b4f3e7ae11431d542cffbf
fuadguluzade <44554892+fuadguluzade@users.noreply.github.com>
2019-10-30 21:39:47 -0700

    Updates

---
+++ precache-manifest.c67180e5b63f0bd9394b6aa24f587c63.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34757abd884bed8d55698d3707692d4e",
+    "url": "/clicky-game-fuad-guluzade/index.html"
+  },
+  {
+    "revision": "6f6394e0ec5ae833cc09",
+    "url": "/clicky-game-fuad-guluzade/static/css/main.a2083c18.chunk.css"
+  },
+  {
+    "revision": "bc8ecfd8d156e26e3bdf",
+    "url": "/clicky-game-fuad-guluzade/static/js/2.6057d209.chunk.js"
+  },
+  {
+    "revision": "6f6394e0ec5ae833cc09",
+    "url": "/clicky-game-fuad-guluzade/static/js/main.1e76d4cb.chunk.js"
+  },
+  {
+    "revision": "4aa57d5c41071b76c69d",
+    "url": "/clicky-game-fuad-guluzade/static/js/runtime-main.b752b1d9.js"
+  }
+]);
